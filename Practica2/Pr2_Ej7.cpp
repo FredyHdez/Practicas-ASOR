69c4954c39ce0db5ebe205908bde1fe5a0ba58de
FredyHdez <fredy.h.b.91@gmail.com>
2020-12-14 10:34:24 +0100

    Corregido el Ej7
    Funcionaba pero en vez de restringir los permisos con el umask se los estaba dando directamente al crearlo con el open

--- Practica2/Pr2_Ej7.cpp
+++ Practica2/Pr2_Ej7.cpp
@@ -7,9 +7,8 @@ int main (){
 
     mode_t umask(027);
 
-    int fichero = open("/home/usuario_vms/Documentos/Pr2_ej7.txt", O_CREAT | O_RDONLY ,0750); 
+    int fichero = open("/home/usuario_vms/Documentos/Pr2_ej7", O_CREAT,0777); 
 
-    //printf("Descriptor: %i \n", fichero);
 
     return 1;
 }
