bc77dc8c8a79fc9b1128811e0e1e15b0de110c14
FredyHdez <fredy.h.b.91@gmail.com>
2021-01-11 10:42:34 +0100

    Ejercicio 5 corregido

--- Practica 3/Pr3_Ej5.cpp
+++ Practica 3/Pr3_Ej5.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
 
 
 int main(int argc, char *argv[]) {
@@ -13,11 +16,14 @@ int main(int argc, char *argv[]) {
 	pid_t psid;
 	char* dirTrab;
 	char* buff;
+	struct rlimit limit;
 
 	pid = getpid();
 	ppid = getppid();
 	pgid = getpgid(pid);
 	psid = getsid(pid);
+
+	getrlimit(RLIMIT_NOFILE, &limit);
 
 	buff = (char*)malloc(sizeof(char) * 5000);
 	dirTrab = getcwd(buff, 5000);
@@ -27,6 +33,7 @@ int main(int argc, char *argv[]) {
 	printf("El identificador del proceso padre es: %i \n", ppid);
 	printf("El identificador del grupo de procesos es: %i \n", pgid);
 	printf("El identificador de sesion es: %i \n", psid);
+	printf("El numero maximo de ficheros es: %i \n", limit.rlim_max);
 	printf("El directorio de trabajo actual es: %s \n", dirTrab);
 
 	free(buff);
